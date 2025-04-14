7c7adfc6d178133dbf0c9b829824ae12613eb3b3
Muhanna asker khan <muhanna1369@gmail.com>
2025-04-14 23:33:49 +0200

    audio issue fix

--- script/main.js
+++ script/main.js
@@ -1,22 +1,15 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
-    Swal.fire({
-        title: 'Do you want to play music in the background?',
-        icon: 'warning',
-        showCancelButton: true,
-        confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
-    }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
-    });
+    const urlParams = new URLSearchParams(window.location.search);
+    const shouldPlay = urlParams.get('music');
+
+    if (shouldPlay === 'yes') {
+        document.querySelector('.song').play();
+    }
+
+    animationTimeline(); // run this regardless
 });
+
 
 
 // animation timeline
